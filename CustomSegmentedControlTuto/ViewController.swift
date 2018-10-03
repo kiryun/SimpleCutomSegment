01ca83f7ecb14d7591005eeedf35f42d615d3a4f
kiryun <rlgus1205@naver.com>
2018-10-03 11:13:19 +0900

    Initial Commit

---
+++ CustomSegmentedControlTuto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomSegmentedControlTuto
+//
+//  Created by 김기현 on 03/10/2018.
+//  Copyright © 2018 김기현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
