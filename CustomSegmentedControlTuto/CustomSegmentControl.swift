45c0a236cea758fe0577feb6519e2782a817126f
kiryun <rlgus1205@naver.com>
2018-10-03 21:14:51 +0900

    first commit

---
+++ CustomSegmentedControlTuto/CustomSegmentControl.swift
@@ -0,0 +1,21 @@
+//
+//  CustomSegmentControl.swift
+//  CustomSegmentedControlTuto
+//
+//  Created by 김기현 on 03/10/2018.
+//  Copyright © 2018 김기현. All rights reserved.
+//
+
+import UIKit
+
+class CustomSegmentControl: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
